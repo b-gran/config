3354c3e26dbd00160e86bb5d88c9a8974074dcda
Bill <bill@improbable.io>
2018-01-25 11:44:21 +0000

    Add misc utils

--- .config/bash/utils.sh
+++ .config/bash/utils.sh
@@ -4,3 +4,25 @@ function ScriptDir {
 
 # Add the utilities in the utils/ directory to the path.
 export PATH=$PATH:$(ScriptDir)/utils
+
+function killport {
+  kill -9 $(lsof -ti tcp:$1)
+}
+
+function searchport {
+  lsof -i tcp:$1
+}
+
+function gitBranchName {
+  git rev-parse --abbrev-ref HEAD 2>/dev/null
+}
+
+function recentBranches {
+  git branch --sort=committerdate
+}
+
+function lastBranch {
+  currentBranchName=$(gitBranchName)
+  mostRecentBranch=$(git branch --sort=committerdate | grep -v $currentBranchName | tail -n 1)
+  git checkout ${mostRecentBranch}
+}
